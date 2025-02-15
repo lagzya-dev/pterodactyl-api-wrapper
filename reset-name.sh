a3a8661b38df850514c0be8d1943e6669b1be6fc
cptcr <tony@localhost.localdomain>
2025-02-15 18:59:51 +0100

    Remove Shell scripts used for NPM publishing

--- reset-name.sh
+++
@@ -1,2 +0,0 @@
-echo 'jq ".name=\"pterodactyl-api-wrapper\"" package.json > package.tmp.json && mv package.tmp.json package.json' > reset-name.sh
-chmod +x reset-name.sh
--- set-github.sh
+++
@@ -1,2 +0,0 @@
-echo 'jq ".name=\"@cptcr/pterodactyl-api-wrapper\"" package.json > package.tmp.json && mv package.tmp.json package.json' > set-github.sh
-chmod +x set-github.sh
